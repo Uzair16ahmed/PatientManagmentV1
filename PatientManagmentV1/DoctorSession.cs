04fadf09e763ba07845a8237fddb0dbefc97e088
Uzair16ahmed <khanuzair285@gmail.com>
2024-05-04 02:41:59 +0500

    DoctorSession created for having doctor data at every form.

---
+++ PatientManagmentV1/DoctorSession.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatientManagmentV1
+{
+    public static class DoctorSession
+    {
+        public static int DoctorId { get; set; }
+        public static string? DoctorName { get; set; }
+        public static string? Role { get; set; }
+
+        // Method to clear the session data
+        public static void ClearSession()
+        {
+            DoctorId = 0;
+            DoctorName = null;
+            Role = null;
+        }
+    }
+}
