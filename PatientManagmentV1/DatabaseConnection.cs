d1bb4ef692c7b3c2af78778eadaddfb74ec6226f
Uzair16ahmed <khanuzair285@gmail.com>
2024-05-14 22:09:26 +0500

    Feat: db connection made available overall

---
+++ PatientManagmentV1/DatabaseConnection.cs
@@ -0,0 +1,22 @@
+﻿using PatientManagmentV1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace PatientManagmentV1
+{
+    public static class DatabaseConnection
+    {
+        private static readonly string connectionString = @"Data Source=DESKTOP-N615JQS\SQLEXPRESS01;Initial Catalog=Patient_ManagementV1;Integrated Security=True;Trust Server Certificate=True";
+
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+
+}
+
